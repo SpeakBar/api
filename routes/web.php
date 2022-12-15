26f33d967831890439dd31e0c4703361c76fa249
Nirbose <nirbose2@gmail.com>
2022-12-15 23:09:18 +0100

    first setup api

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        "api" => "speakbar-api",
+        "version" => "0.0.1v"
+    ]);
 });
